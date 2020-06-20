71f2d1bba70ba84ecfd9bbf30bef2b31304c0775
Brooke G <brooke.a.guarienti@gmail.com>
2020-06-19 23:22:09 -0500

    added components to src folder, including header, footer, and navbar index.js files

---
+++ clicky-game/src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./style.css";
+
+function Header() {
+    return(
+        <div className="header">
+            <h2>
+                Gardening Click-Game
+            <br />
+            Be sure to click on a plant only ONCE! 
+            <br />
+            If you click on a plant more than once, you lose!
+            </h2>
+
+        </div>
+    );
+}
+
+export default Header;
